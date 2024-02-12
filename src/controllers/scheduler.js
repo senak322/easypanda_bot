a2a2e1edc6e74fbe0c9ac603b5ee40746b585aa5
Aleksandr Viznyi <senak98@yandex.ru>
2024-02-12 15:13:26 +0800

    add messanger

--- src/controllers/scheduler.js
+++ src/controllers/scheduler.js
@@ -1,7 +1,8 @@
 import cron from "node-cron";
 import Order from "../models/ExchangeOrder.js"; // Убедитесь, что путь к модели верный
 import { config } from "../../config.js";
-const { adminChatId } = config;
+import axios from "axios";
+const { adminChatId, currencyRubUrl, currencyCnyUrl, currencyUahUrl } = config;
 
 const checkExpiredOrders = async (bot) => {
   try {
@@ -27,9 +28,61 @@ const checkExpiredOrders = async (bot) => {
   }
 };
 
+// Функция для получения курсов валют и добавления вашего процента
+async function getCustomExchangeRates() {
+  const rubCurr = await axios.get(currencyRubUrl);
+  const uahCurr = await axios.get(currencyUahUrl);
+  const cnyCurr = await axios.get(currencyCnyUrl);
+
+  return {
+    RUB_CNY: rubCurr.data.rub.cny * 1.07,
+    UAH_CNY: uahCurr.data.uah.cny * 1.1,
+    CNY_RUB: cnyCurr.data.cny.rub * 0.96,
+    CNY_UAH: cnyCurr.data.cny.uah * 0.96,
+    // Добавьте другие пары валют по аналогии
+  };
+}
+
+
+
+// Функция для отправки сообщения с приветствием и курсами валют
+async function sendGreetingAndRates(bot) {
+  const greetings = [
+    "Доброе утро! Хорошего начала дня!🎉",
+    "Добрый день! Желаем продуктивной второй половины дня!",
+    "Доброй ночи, желаем крепкого сна!",
+  ];
+
+  // Получаем текущий час для определения нужного приветствия
+  const currentHour = new Date().getHours();
+  const greetingIndex = currentHour < 12 ? 0 : currentHour < 18 ? 1 : 2;
+
+  const rates = await getCustomExchangeRates();
+  const ratesMessage = `RUB🇷🇺 -> CNY🇨🇳 ${rates.RUB_CNY}\nUAH🇺🇦 -> CNY🇨🇳 ${rates.UAH_CNY}\nCNY🇨🇳 -> RUB🇷🇺 ${rates.CNY_RUB}\nCNY🇨🇳 -> UAH🇺🇦 ${rates.CNY_UAH}\n`;
+
+  const message = `${greetings[greetingIndex]}\n\n🚀Экспресс обмен валют💸\nГорячий курс🔥\nКурс на момент публикации:\n${ratesMessage}\n\nПроверь актуальный курс в боте, либо на сайте!\n...`;
+
+  bot.telegram.sendMessage(config.adminChatId, message, {
+    parse_mode: "Markdown",
+    disable_web_page_preview: true,
+  });
+}
+
 export const startCronJobs = (bot) => {
   // Задача на проверку каждую минуту
   cron.schedule("* * * * *", () => checkExpiredOrders(bot));
-};
 
-// module.exports = startCronJobs;
+  // Отправка приветствия и курсов валют три раза в день
+  cron.schedule("0 9 * * *", () => sendGreetingAndRates(bot), {
+    scheduled: true,
+    timezone: "Europe/Moscow",
+  });
+  cron.schedule("0 15 * * *", () => sendGreetingAndRates(bot), {
+    scheduled: true,
+    timezone: "Europe/Moscow",
+  });
+  cron.schedule("0 21 * * *", () => sendGreetingAndRates(bot), {
+    scheduled: true,
+    timezone: "Europe/Moscow",
+  });
+};
